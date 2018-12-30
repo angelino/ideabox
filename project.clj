c8771df6d35fef3b7c1215793e4e47b1595fdc1a
Lucas Angelino dos Santos <lucas.santos@synchro.com.br>
2018-12-30 18:02:48 -0200

    Add postgres driver as default dependency

--- project.clj
+++ project.clj
@@ -9,13 +9,13 @@
                  [hiccup "1.0.5"]
                  [bouncer "1.0.1"]
                  [environ "1.1.0"]
-                 [org.clojure/java.jdbc "0.7.8"]]
+                 [org.clojure/java.jdbc "0.7.8"]
+                 [org.postgresql/postgresql "42.2.5"]]
   :plugins [[lein-ring "0.12.4"]
             [lein-environ "1.1.0"]]
   :profiles {;;:dev {:dependencies [[com.h2database/h2 "1.4.197"]]}
              ;; see: https://devcenter.heroku.com/articles/deploying-clojure
-             :uberjar {:aot :all
-                       :dependencies [[org.postgresql/postgresql "42.2.5"]]}
+             :uberjar {:aot :all}
              :production {:env {:production true}}}
   :uberjar-name "ideabox-standalone.jar"
   :min-lein-version "2.0.0"

96507b567c29df79f7e315de49403bee07c38367
Lucas Angelino dos Santos <lucas.santos@synchro.com.br>
2018-12-30 17:59:10 -0200

    Add postgres driver as uberjar dependency

--- project.clj
+++ project.clj
@@ -14,9 +14,9 @@
             [lein-environ "1.1.0"]]
   :profiles {;;:dev {:dependencies [[com.h2database/h2 "1.4.197"]]}
              ;; see: https://devcenter.heroku.com/articles/deploying-clojure
-             :uberjar {:aot :all}
-             :production {:env {:production true}
-                          :dependencies [[org.postgresql/postgresql "42.2.5"]]}}
+             :uberjar {:aot :all
+                       :dependencies [[org.postgresql/postgresql "42.2.5"]]}
+             :production {:env {:production true}}}
   :uberjar-name "ideabox-standalone.jar"
   :min-lein-version "2.0.0"
   :main ideabox.core

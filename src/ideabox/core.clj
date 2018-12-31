b46bc554810b137163068d46c3ec0ce4f318952f
Lucas Angelino dos Santos <lucas.santos@synchro.com.br>
2018-12-31 14:06:02 -0200

    Uses the JDBC_DATABASE_URL when available in order to deploy at Heroku

--- src/ideabox/core.clj
+++ src/ideabox/core.clj
@@ -39,11 +39,14 @@
       (handler (assoc req :request-method method))
       (handler req))))
 
+(def db (or (env :jdbc-database-url) ;; see: https://devcenter.heroku.com/articles/connecting-to-relational-databases-on-heroku-with-java#using-the-jdbc_database_url
+            {:connection-uri (env :database-connection-uri)
+             :user (env :database-user)
+             :password (env :database-password)}))
+
 (defn wrap-database [handler]
   (fn [req]
-    (handler (assoc req :ideabox/db {:connection-uri (env :database-url)
-                                     :user (env :database-user)
-                                     :password (env :database-password)}))))
+    (handler (assoc req :ideabox/db db))))
 
 ;; App config
 

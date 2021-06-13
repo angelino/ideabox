c5ecaaba295346c8b02f12f6d69c66fb5008943d
Lucas Angelino dos Santos <lucas.angelino@gmail.com>
2021-06-13 20:10:06 -0300

    Update libs

--- project.clj
+++ project.clj
@@ -3,16 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [ring "1.7.1"]
-                 [compojure "1.6.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [ring "1.9.3"]
+                 [compojure "1.6.2"]
                  [hiccup "1.0.5"]
-                 [buddy/buddy-auth "2.1.0"]
-                 [buddy/buddy-hashers "1.3.0"]
+                 [buddy/buddy-auth "3.0.1"]
+                 [buddy/buddy-hashers "1.8.1"]
                  [bouncer "1.0.1"]
-                 [environ "1.1.0"]
-                 [org.clojure/java.jdbc "0.7.8"]
-                 [org.postgresql/postgresql "42.2.5"]]
+                 [environ "1.2.0"]
+                 [org.clojure/java.jdbc "0.7.12"]
+                 [org.postgresql/postgresql "42.2.21"]]
   :plugins [[lein-ring "0.12.5"]
             [lein-environ "1.1.0"]]
   :profiles {;; see: https://devcenter.heroku.com/articles/deploying-clojure

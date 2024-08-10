c0e6690ba3b09e6cf6dcafccfa3658bdae9afd46
Lucas Angelino dos Santos <lucas.angelino@gmail.com>
2024-08-10 02:08:21 -0300

    Add lib Telemere for logs

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.4"]
+                 [com.taoensso/telemere "1.0.0-beta16"]
+                 [com.taoensso/slf4j-telemere "1.0.0-beta16"]
                  [ring "1.12.2"]
                  [compojure "1.7.0"] ; 1.7.1 with ring 1.12.2 it is broken
                  [hiccup "1.0.5"]

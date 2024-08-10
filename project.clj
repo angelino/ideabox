f878fbc9248d03f03bc49adf0dff214684e68729
Lucas Angelino <angelino@Lucass-MacBook-Pro.local>
2024-08-10 01:56:34 -0300

    Bump lib versions

--- project.clj
+++ project.clj
@@ -3,17 +3,17 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [ring "1.9.3"]
-                 [compojure "1.6.2"]
+  :dependencies [[org.clojure/clojure "1.11.4"]
+                 [ring "1.12.2"]
+                 [compojure "1.7.0"] ; 1.7.1 with ring 1.12.2 it is broken
                  [hiccup "1.0.5"]
-                 [buddy/buddy-auth "3.0.1"]
-                 [buddy/buddy-hashers "1.8.1"]
+                 [buddy/buddy-auth "3.0.323"]
+                 [buddy/buddy-hashers "2.0.167"]
                  [bouncer "1.0.1"]
                  [environ "1.2.0"]
                  [org.clojure/java.jdbc "0.7.12"]
-                 [com.h2database/h2 "2.3.230"]
-                 [org.postgresql/postgresql "42.2.21"]]
+                 #_[com.h2database/h2 "2.3.230"]
+                 [org.postgresql/postgresql "42.7.3"]]
   :plugins [[lein-ring "0.12.5"]
             [lein-environ "1.1.0"]
             [lein-codox "0.10.8"]]
